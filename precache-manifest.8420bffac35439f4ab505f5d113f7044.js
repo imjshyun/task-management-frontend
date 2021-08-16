37781110b9a3237abdbde5b0aef606d69025e340
imjshyun <imjshyun@users.noreply.github.com>
2021-08-16 20:39:46 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.8420bffac35439f4ab505f5d113f7044.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a7a06e0ea3da9fb64b847103508a6a",
+    "url": "https://imjshyun.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "d632e2e398ee4b6c6edf",
+    "url": "https://imjshyun.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "57b9acda8f89cd471fae",
+    "url": "https://imjshyun.github.io/task-management-frontend/static/js/2.4dc0c0df.chunk.js"
+  },
+  {
+    "revision": "d632e2e398ee4b6c6edf",
+    "url": "https://imjshyun.github.io/task-management-frontend/static/js/main.a7e7aa66.chunk.js"
+  },
+  {
+    "revision": "04b02859e030cba34675",
+    "url": "https://imjshyun.github.io/task-management-frontend/static/js/runtime~main.e91caa98.js"
+  }
+]);
